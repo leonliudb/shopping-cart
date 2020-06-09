927bd09cc0eb743bb8bf120c7aa0874dda01867c
leonliudb <dl2868@nyu.edu>
2020-06-09 02:50:58 -0700

    Capture user input

--- shopping_cart.py
+++ shopping_cart.py
@@ -23,6 +23,16 @@ products = [
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
+
+
+selected_id = input("Please input a product identifier: ")
+print(selected_id)
+print(type(selected_id))
+
+
+
+
+
 def to_usd(my_price):
     """
     Converts a numeric value to usd-formatted string, for printing and display purposes.
@@ -37,5 +47,5 @@ def to_usd(my_price):
 
 # TODO: write some Python code here to produce the desired output
 
-print(products)
+#print(products)
 
